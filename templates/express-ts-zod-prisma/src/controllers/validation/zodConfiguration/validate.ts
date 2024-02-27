21ad50add1f40d56b6dd60c7773208a2c9c6aa64
mohammed al mallahi <x52-@hotmail.com>
2024-02-27 18:49:13 +0300

    start first template backend ready

---
+++ templates/express-ts-zod-prisma/src/controllers/validation/zodConfiguration/validate.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
+import {z, ZodError} from 'zod'
+
+export const validator =(schema:z.AnyZodObject)=>(req:Request,res:Response,next:NextFunction)=>{
+    try{
+        schema.parse({
+            body:req.body,
+            params:req.params,
+            query:req.query
+        })
+        next();
+    }
+    catch(err){
+        const zodError=err as ZodError;
+        return res.status(400).json({msg:{
+            error:zodError.errors[0].message,
+            code:zodError.errors[0].code
+        }})
+    }
+}
